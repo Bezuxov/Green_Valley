83dd94d404e3bf65f478a4e4f1617afec7c87133
Ivan <Vanka178@mail.ru>
2016-12-19 23:13:18 +0300

    Сломал гит и хедер. Разметка 75%

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+$(".burger-menu").on('click', function () {
+//$(this).toggleClass("menu-on");
+  $('[data-menu="mobile"]').toggleClass('main-nav--hide');
+
+
+(function($contactTab) {
+        if (!$contactTab.length) {
+            return;
+        }
+
+        var $link = $contactTab;
+        var $tab = $('.promo-img-slider');
+
+        $link.on('click', function(e) {
+            var $self = $(this);
+            var index = $self.index();
+
+            e.preventDefault();
+
+            if (!$self.hasClass('is-active')) {
+                $link.removeClass('is-active');
+                $self.addClass('is-active');
+            }
+            $tab.hide();
+            $tab.eq(index).show();
+        });
+    })($('tabs-slider__link'));
+
+});
